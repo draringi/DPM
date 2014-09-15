e43c20d8b70e80d85927eb69e083e629f206dc5d
leosiracusa <leonardo.siracusa@mail.mcgill.ca>
2014-09-15 12:58:22 -0400

    Update PController.java

--- Lab 1/PController.java
+++ Lab 1/PController.java
@@ -3,11 +3,11 @@ import lejos.nxt.*;
 public class PController implements UltrasonicController {
 	
 	private final int bandCenter, bandwith;
-	private final int motorStraight = 200, FILTER_OUT = 20;
+	private final int motorStraight = 200;
 	private final NXTRegulatedMotor leftMotor = Motor.A, rightMotor = Motor.C;	
 	private int distance;
 	private int currentLeftSpeed;
-	private int filterControl;
+	
 	
 	public PController(int bandCenter, int bandwith) {
 		//Default Constructor
@@ -18,24 +18,13 @@ public class PController implements UltrasonicController {
 		leftMotor.forward();
 		rightMotor.forward();
 		currentLeftSpeed = 0;
-		filterControl = 0;
+		
 	}
 	
 	@Override
 	public void processUSData(int distance) {
 		
-		// rudimentary filter
-		if (distance == 255 && filterControl < FILTER_OUT) {
-			// bad value, do not set the distance var, however do increment the filter value
-			filterControl ++;
-		} else if (distance == 255){
-			// true 255, therefore set distance to 255
-			this.distance = distance;
-		} else {
-			// distance went below 255, therefore reset everything.
-			filterControl = 0;
-			this.distance = distance;
-		}
+		
 		// TODO: process a movement based on the us distance passed in (P style)
 		
 	}
