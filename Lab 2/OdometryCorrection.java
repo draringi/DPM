adcfad4efd9ea506def6d7aa3e052e6802184045
Michael Williams <draringi@draringi.net>
2014-09-22 18:23:23 -0400

    Have theoretecally great corrector. About to test.

--- Lab 2/OdometryCorrection.java
+++ Lab 2/OdometryCorrection.java
@@ -1,22 +1,39 @@
 /* 
  * OdometryCorrection.java
  */
+import lejos.nxt.*;
 
 public class OdometryCorrection extends Thread {
 	private static final long CORRECTION_PERIOD = 10;
 	private Odometer odometer;
-
+	private ColorSensor sensor;
+    private int oldValue;
+	
 	// constructor
 	public OdometryCorrection(Odometer odometer) {
 		this.odometer = odometer;
+		sensor = new ColorSensor(SensorPort.S1);
+		oldValue = sensor.getNormalizedLightValue();
+		Sound.setVolume(100);
 	}
 
 	// run method (required for Thread)
 	public void run() {
 		long correctionStart, correctionEnd;
-
+		int value;
+		
 		while (true) {
 			correctionStart = System.currentTimeMillis();
+			value = sensor.getRawLightValue();
+			if(value < 400){
+				Sound.beepSequence();
+				double theta = odometer.getTheta();
+				if(isX(theta)){
+					odometer.setX(nearestLine(odometer.getX()));
+				} else {
+					odometer.setY(nearestLine(odometer.getY()));
+				}
+			}
 
 			// put your correction code here
 
@@ -33,5 +50,16 @@ public class OdometryCorrection extends Thread {
 				}
 			}
 		}
+	}
+	
+	boolean isX(double theta){
+		double val = theta % Math.PI;
+		return val < Math.PI/4 || val > 3*Math.PI/4; 
+	}
+	
+	double nearestLine(double current){
+		double roughCount = (current - 15)/30;
+		long count = Math.round(roughCount);
+		return (double) count * 30 + 15;
 	}
 }
