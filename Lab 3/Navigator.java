60753d67590f3c65f0e9eba6d4e26cfb2fe4fd45
Michael Williams <draringi@draringi.net>
2014-09-29 13:07:27 -0400

    Added some Turning logiic
    Turning Turning Turning

--- Lab 3/Navigator.java
+++ Lab 3/Navigator.java
@@ -4,19 +4,45 @@ import java.util.Queue;
 class Navigator implements TimerListener {
 	private Odometer odometer;
 	private boolean travelling;
+	private boolean turning;
+	static private final TOLERANCE = 0.1;
+	static private final MIN_ANGLE = Math.PI/16;
+	static private final FORWARD_SPEED = 250;
+	static private final TURNING_SPEED = 100;
 	private Object lock;
-	private double xTarget, yTarget;
+	private double xTarget, yTarget, targetTheta;
+	private final NXTRegulatedMotor leftMotor = Motor.A, rightMotor = Motor.B;
+	
 	
 	public Navigator () {
 		this.odometer = new Odometer();
 		this.travelling = false;
+		this.turning = false;
 	}
 	
 	public void timedOut() {
 	    synchronized (lock) {
-	    	if(this.travelling){
+	    	if(this.turning){
 	    		
-	    	}	
+	    	}
+	    	if(this.travelling){
+	    		if(true) {
+	    			double xDiff = xTarget - odometer.getX();
+	    			double yDiff = yTarget - odometer.getY();
+	    			if (Math.abs(xDiff) < TOLERANCE && Math.abs(yDiff) < TOLERANCE){
+	    				this.traveling = false;
+	    				engineStop();
+	    				break;
+	    			}
+	    			targetTheta = Math.atan(yDiff/xDiff);
+	    			if (x < 0){
+	    				targetTheta += Math.PI;
+	    			}
+	    			if( targetTheta - odometer.getTheta() < MIN_ANGLE ){
+	    				engineStart();
+	    			}
+	    		}
+	    	}
 	    }
 	}
 	
@@ -30,7 +56,8 @@ class Navigator implements TimerListener {
 	
 	public void turnTo(double theta){
 		synchronized (lock) {
-			
+			this.targetTheta = theta;
+			this.turning = true;
 		}
 	}
 	
@@ -40,5 +67,35 @@ class Navigator implements TimerListener {
 			result = this.travelling;
 		}
 		return result;
+	}
+	
+	private void engineStop(){
+		leftMotor.setSpeed(0);
+		rightMotor.setSpeed(0);
+	}
+	
+	private void engineStart(){
+		leftMotor.setSpeed(FORWARD_SPEED);
+		rightMotor.setSpeed(FORWARD_SPEED);
+		leftMotor.forward()
+		rightMotor.forward()
+	}
+	
+	private void turnLeft(int turnAngle){
+		leftMotor.setSpeed(TURNING_SPEED);
+		rightMotor.setSpeed(TURNING_SPEED);
+		leftMotor.rotate(-turnAngle, true);
+		rightMotor.rotate(turnAngle, false);
+	}
+	
+	private void turnRight(int turnAngle){
+		leftMotor.setSpeed(TURNING_SPEED);
+		rightMotor.setSpeed(TURNING_SPEED);
+		leftMotor.rotate(turnAngle, true)
+		rightMotor.rotate(-turnAngle, false)
+	}
+	
+	private int convertAngle(double angle) {
+		return WIDTH * angle * 180/(2*Math.PI*RADIUS);
 	}
 }
