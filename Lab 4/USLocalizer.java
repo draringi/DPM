14976e00340dc3e6d74a3fc1dcb060627c6b874d
Michael Williams <draringi@draringi.net>
2014-10-07 11:20:40 -0400

    Implemented Falling-edge

--- Lab 4/USLocalizer.java
+++ Lab 4/USLocalizer.java
@@ -2,12 +2,14 @@ import lejos.nxt.UltrasonicSensor;
 
 public class USLocalizer {
 	public enum LocalizationType { FALLING_EDGE, RISING_EDGE };
-	public static double ROTATION_SPEED = 30;
+	public static final double ROTATION_SPEED = 30, STOP = 0;
 
 	private Odometer odo;
 	private TwoWheeledRobot robot;
 	private UltrasonicSensor us;
 	private LocalizationType locType;
+	
+	private static final int D = 30, K = 2;
 	
 	public USLocalizer(Odometer odo, UltrasonicSensor us, LocalizationType locType) {
 		this.odo = odo;
@@ -25,18 +27,24 @@ public class USLocalizer {
 		
 		if (locType == LocalizationType.FALLING_EDGE) {
 			// rotate the robot until it sees no wall
-			
+			robot.setRotationSpeed(ROTATION_SPEED);
+			while(getFilteredData() < D);
 			// keep rotating until the robot sees a wall, then latch the angle
-			
+			while(getFilteredData() > D);
+			angleA = odo.getAngle();
 			// switch direction and wait until it sees no wall
-			
+			robot.setRotationSpeed(-ROTATION_SPEED);
+			while(getFilteredData() < D);
 			// keep rotating until the robot sees a wall, then latch the angle
-			
+			while(getFilteredData() > D);
+			angleB = odo.getAngle();
+			robot.setRotationSpeed(STOP);
 			// angleA is clockwise from angleB, so assume the average of the
 			// angles to the right of angleB is 45 degrees past 'north'
-			
+			double diagonalPoint = Odometer.fixDegAngle((angleA + angleB)/2);
 			// update the odometer position (example to follow:)
-			odo.setPosition(new double [] {0.0, 0.0, 0.0}, new boolean [] {true, true, true});
+			double theta = Odometer.fixDegAngle(odo.getAngle() - diagonalPoint + 45);
+			odo.setPosition(new double [] {-15.0, -15.0, theta}, new boolean [] {true, true, true});
 		} else {
 			/*
 			 * The robot should turn until it sees the wall, then look for the
@@ -45,9 +53,7 @@ public class USLocalizer {
 			 * will face toward the wall for most of it.
 			 */
 			
-			//
-			// FILL THIS IN
-			//
+
 		}
 	}
 	
