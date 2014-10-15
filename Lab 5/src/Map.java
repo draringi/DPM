40dd52f91073c123ae4c70046272bad8b353724d
Michael Williams <draringi@draringi.net>
2014-10-15 11:52:02 -0400

    Added Map class, and added default map factory.

---
+++ Lab 5/src/Map.java
@@ -0,0 +1,40 @@
+import java.util.BitSet;
+public class Map {
+	private BitSet bitset; 
+	private int width, height;
+	
+	public Map(int height, int width) {
+		this.height = height;
+		this.width = width;
+		bitset = new BitSet(width*height);
+	}
+
+	public void clear(){
+		bitset.clear();
+	}
+	
+	public void clear(int x, int y){
+		bitset.clear(getIndex(x,y));
+	}
+	
+	public void set(int x, int y){
+		bitset.set(getIndex(x,y));
+	}
+	
+	public static Map DefaultMap(){
+		Map map = new Map(4, 4);
+		map.set(1,0);
+		map.set(0, 3);
+		map.set(2, 2);
+		map.set(3,2);
+		return map;
+	}
+	
+	public boolean blocked(int x, int y){
+		return bitset.get(getIndex(x,y));
+	}
+	
+	private int getIndex(int x, int y){
+		return y*width+x;
+	}
+}
