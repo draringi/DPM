13dd9b0a24bca69f8d164c6f42cd011561e5c537
Michael Williams <draringi@draringi.net>
2014-10-17 13:35:22 -0400

    Update Map.java

--- Lab 5/src/Map.java
+++ Lab 5/src/Map.java
@@ -1,5 +1,11 @@
 import java.util.BitSet;
 
+/**
+ * Map stores map data, and performs calculations between odometer data and map-tile data
+ * @author Michael Williams (260369438)
+ * @author Leonardo Siracusa (260585931)
+ *
+ */
 public class Map {
 	private BitSet bitset; 
 	private int width, height;
@@ -11,18 +17,31 @@ public class Map {
 		this.bitset.clear();
 	}
 
+	/**
+	 * Clears the map
+	 */
 	public void clear(){
 		bitset.clear();
 	}
 	
+	/**
+	 * Clears a point on the map
+	 */
 	public void clear(int x, int y){
 		bitset.clear(getIndex(x, y));
 	}
 	
+	
+	/**
+	 * Sets a point on the map
+	 */
 	public void set(int x, int y){
 		bitset.set(getIndex(x, y));
 	}
 	
+	/**
+	 * Factory for the default map for Lab 5
+	 */
 	public static Map DefaultMap(){
 		Map map = new Map(4, 4);
 		map.set(1, 0);
@@ -32,6 +51,9 @@ public class Map {
 		return map;
 	}
 	
+	/**
+	 * 
+	 */
 	public boolean blocked(int x, int y){
 		if(!(valid(x, y))){
 			return true;
@@ -39,26 +61,44 @@ public class Map {
 		return bitset.get(getIndex(x, y));
 	}
 	
+	/**
+	 * Converts 2 dimensional co-ordinates into linear index value
+	 */
 	public int getIndex(int x, int y){
 		return y*width+x;
 	}
 	
+	/**
+	 * Returns the height of the map, as set at initialization
+	 */
 	public int getHeight(){
 		return this.height;
 	}
 	
+	/**
+	 * Returns the width of the map, as set at initialization
+	 */
 	public int getWidth(){
 		return this.width;
 	}
 	
+	/**
+	 * Converts double from the odometer system into grid id value
+	 */
 	public int getGrid(double val){
 		return getGrid(val, false);
 	}
 	
+	/**
+	 * Converts grid-id value into odometer system value
+	 */
 	public double getPos(int val){
 		return (double) val*30.0 - 15;
 	}
 	
+	/**
+	 * Converts grid co-ordinates into odometer system co-ordinates
+	 */
 	public double [] getPos(int [] val){
 		double [] pos = new double [2];
 		pos[Odometer.X] = getPos(val[Odometer.X]);
@@ -66,6 +106,9 @@ public class Map {
 		return pos;
 	}
 	
+	/**
+	 * Converts double from the odometer system into grid id value
+	 */
 	public int getGrid(double val, boolean orienteering){
 		val /=30;
 		if(!orienteering){
@@ -74,6 +117,9 @@ public class Map {
 		return (int) Math.round(val);
 	}
 	
+	/**
+	 * Reports if a co-ordinate set is a valid spot on the map
+	 */
 	public boolean valid(int x, int y){
 		return (x >= 0 && x < width && y >= 0 && y < height);
 	}
