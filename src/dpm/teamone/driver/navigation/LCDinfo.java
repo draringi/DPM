7387bdea6c13b502cfcd7377abfcf473dde060f3
Michael Williams <draringi@draringi.net>
2014-11-09 00:51:45 -0500

    Added LCDinfo

---
+++ src/dpm/teamone/driver/navigation/LCDinfo.java
@@ -0,0 +1,24 @@
+package dpm.teamone.driver.navigation;
+import dpm.teamone.driver.DriverRobot;
+import lejos.nxt.LCD;
+public class LCDinfo {
+
+	protected LCDinfo() {
+		LCD.drawString("Localizing...", 0, 0);
+	}
+	
+	protected void setStartPos(int pos[]){
+		Direction dir = Direction.toDirection(pos[DriverRobot.POS_THETA]);
+		setStartPos(pos[DriverRobot.POS_X], pos[DriverRobot.POS_Y], dir);
+	}
+	
+	protected void setStartPos(int x, int y, Direction dir){
+		LCD.drawString("X: ", 0, 0);
+		LCD.drawString("Y: ", 0, 1);
+		LCD.drawString("H: ", 0, 2);
+		LCD.drawInt(x, 3, 0);
+		LCD.drawInt(y, 3, 1);
+		LCD.drawString(dir.toString(), 3, 2);
+	}
+
+}
