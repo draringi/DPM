95eeb3a1f76ecf52ea9d0123c349a2aaa796ab77
Michael Williams <draringi@draringi.net>
2014-11-18 19:53:44 -0500

    set up for Beta map 1

--- src/dpm/teamone/driver/DriverRobot.java
+++ src/dpm/teamone/driver/DriverRobot.java
@@ -34,12 +34,12 @@ public class DriverRobot {
 		mapData[MAP_DATA_MAP] = BETA_MAP;
 		comms.prepareTravel();
 		//map = MapFactory.getMap(mapData[MAP_DATA_MAP]);
-		//map = MapFactory.getBetaMap(mapData[MAP_DATA_MAP]);
-		map = MapFactory.lab5Map();
+		map = MapFactory.getBetaMap(mapData[MAP_DATA_MAP]);
+		//map = MapFactory.lab5Map();
 		nav = new NavigationController(map);
 		nav.setDropZone(mapData[MAP_DATA_DROP_X], mapData[MAP_DATA_DROP_Y], 1, 1);
-		nav.setPickUpZone(2, 1, 1, 1); //Lab 5 map
-		//nav.setPickUpZone(2, 2, 0, 0); //Any 8x8 map
+		//nav.setPickUpZone(2, 1, 1, 1); //Lab 5 map
+		nav.setPickUpZone(2, 2, 0, 0); //Any 8x8 map
 		nav.localize();
 		events = new EventManager(nav);
 		events.start();
