821142a2667bd9754dc891d8b36e93e8befb7fd5
Michael Williams <draringi@draringi.net>
2014-12-01 14:59:21 -0500

    Added actual Light Sensor Filter...

---
+++ src/dpm/teamone/driver/navigation/Light.java
@@ -0,0 +1,32 @@
+package dpm.teamone.driver.navigation;
+
+import lejos.nxt.ColorSensor;
+import lejos.nxt.SensorPort;
+
+/**
+ * Averaging filtering Ultrasonic reader
+ * 
+ * @author Michael Williams (260369438)
+ * 
+ */
+public class Light {
+
+	private static final int LOOPS = 40;
+
+	private final ColorSensor sensor;
+
+	public Light(SensorPort port) {
+		// Change this to which ever port contains the US
+		this.sensor = new ColorSensor(port);
+		this.sensor.getFloodlight();
+	}
+
+	public int poll() {
+		int i, sum = 0;
+		for (i = 0; i < LOOPS; i++) {
+			sum += sensor.getRawLightValue();
+		}
+		return sum / LOOPS;
+	}
+
+}
