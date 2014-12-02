e4907cb382f5a37f0a3f0a701feca2f897794404
Michael Williams <draringi@draringi.net>
2014-12-02 11:43:57 -0500

    Latest Calibration

--- src/dpm/teamone/driver/navigation/NavigationController.java
+++ src/dpm/teamone/driver/navigation/NavigationController.java
@@ -31,8 +31,8 @@ public class NavigationController {
 			RIGHT_MOTOR = Motor.B;
 	private static final double ROTATE_SPEED = 30;
 	private static final double TRACK_WIDTH = 19.0;
-	private static final double WHEEL_DIAMETER = 4.18;
-	private static final int ACCELERATION = 30;
+	private static final double WHEEL_DIAMETER = 4.15;
+	private static final int ACCELERATION = 25;
 	public int[] dropZone;
 	public GridMap map;
 	private final Navigator navigator;
